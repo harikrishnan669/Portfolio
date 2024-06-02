4f38329f123ac0db92c3cc2827d1d2e61785b0ab
Harikrishnan R <128880349+harikrishnan669@users.noreply.github.com>
2024-06-02 21:32:06 +0530

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-  const menuIcon = document.getElementById('menu-icon');
-  const navbar = document.querySelector('.navbar');
-
-  menuIcon.addEventListener('click', function() {
-    navbar.classList.toggle('active');
-  });
-});
