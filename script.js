9f7e27931878671b26ea140fe2f741686dccca52
Hari <rharikrishnan2020@gmail.com>
2025-04-11 23:32:35 +0530

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	index.html

--- script.js
+++ script.js
@@ -23,4 +23,30 @@
             });
         });
     });
-  
+  const words = ['Computer Science Undergraduate', 'Designer'];
+       let wordIndex = 0;
+       let charIndex = 0;
+       const typingElement = document.getElementById('typed-text');
+       let isDeleting = false;
+
+       function type() {
+           const currentWord = words[wordIndex];
+           const displayedText = currentWord.substring(0, charIndex);
+           typingElement.textContent = displayedText;
+
+           if (!isDeleting && charIndex < currentWord.length) {
+               charIndex++;
+               setTimeout(type, 100);
+           } else if (isDeleting && charIndex > 0) {
+               charIndex--;
+               setTimeout(type, 50);
+           } else {
+               isDeleting = !isDeleting;
+               if (!isDeleting) {
+                   wordIndex = (wordIndex + 1) % words.length;
+               }
+               setTimeout(type, 1000);
+           }
+       }
+
+       document.addEventListener('DOMContentLoaded', type);
